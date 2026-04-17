b3077d35f97de29220cd14d71977a9ae58e15c7b
weihuo1988-svg <wei.huo1988@gmail.com>
2026-04-17 18:25:29 +0800

    Add projects/sleep-aid/src/main.tsx

---
+++ projects/sleep-aid/src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import { ErrorBoundary } from './components/ErrorBoundary.tsx'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+)
